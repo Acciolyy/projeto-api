45c24f94b3ab0b69046d6bd74ef710e366e5ef87
Joao Moreira <joao_m_felix@outlook.com>
2024-12-06 19:54:09 -0400

    MInha conta e fav

--- filmes-api-orc/src/pages/Favorites.jsx
+++ filmes-api-orc/src/pages/Favorites.jsx
@@ -1,10 +1,60 @@
 import React, { useEffect, useState } from "react";
+import { use } from "react";
 
 const Favorites = () => {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    if (storedUser) {
+      setNome(storedUser.nome);
+      setEmail(storedUser.email);
+      setSenha(storedUser.senha);
+    }
+  }, []);
+
+  const handleSave = (event) => {
+    event.preventDefault();
+
+  const updatedUser = { nome, email, senha};
+  localStorage.setItem("user", JSON.stringify(updatedUser));
+
+  alert("Dados atualizados com sucesso!");
+
+  };
+
   return (
-    <div>
-      <h1>Favoritos</h1>
-      <p>Conteúdo da página de favoritos</p>
+    <div className="profile-container">
+      <h1>Editar Perfil</h1>
+      <form onSubmit={handleSave}>
+        <label>
+          Nome:
+          <input 
+            type="text" 
+            value={nome} 
+            onChange={(e) => setNome(e.target.value)} 
+          />
+        </label>
+        <label>
+          Email:
+          <input 
+            type="email" 
+            value={email} 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+        </label>
+        <label>
+          Senha:
+          <input 
+            type="password" 
+            value={senha} 
+            onChange={(e) => setSenha(e.target.value)} 
+          />
+        </label>
+        <button type="submit">Salvar</button>
+      </form>
     </div>
   );
 };
